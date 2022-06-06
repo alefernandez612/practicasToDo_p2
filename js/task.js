ea5b57a49bbb7b5d684cab7db781cada2f62264e
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-06-06 20:55:08 +0200

    Creadas las clases task.js y task.service.js

--- js/task.js
+++ js/task.js
@@ -1,3 +1,50 @@
-class Task {
+export default class Task {
+    constructor({title, priority, important, completed} = {important: false, completed: false}) {
+        this._id = Date.now();
+        this.title = title;
+        this.priority = priority;
+        this.important = important;
+        this.completed = completed;
+    }
 
+    get id() {
+        this.id;
+    }
+    get title() {
+        this.title;
+    }
+    get priority() {
+        this.priority;
+    }
+    get important() {
+        this.important;
+    }
+    get completed() {
+        this.completed;
+    }
+    set title(title) {
+        title = title.trim();
+        if (!title) {
+            throw new Error("Title cannot be empty.");
+        }
+        this._title = title;
+    }
+    set priority(priority) {
+        if (!priority) {
+            throw new Error("Priority cannot be empty.");
+        }
+        this._priority = priority;
+    }
+    set important(important) {
+        if (typeof important !== 'boolean') {
+            throw new Error("Important must be boolean.");
+        }
+        this._important = important;
+    }
+    set completed(completed) {
+        if (typeof completed !== 'boolean') {
+            throw new Error("Completed must be boolean.");
+        }
+        this._completed = completed;
+    }
 }
