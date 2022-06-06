8b0bdd6f600b4b601bdfa9661ad967b5b0bcb291
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-06-06 22:47:37 +0200

    Modificación del archivo task.services.js

--- js/task.service.js
+++ js/task.service.js
@@ -4,17 +4,31 @@ class TaskService {
         this.tasks = tasks.map(task => new Task(task));
     }
 
+    bindTaskListChanged(callback) {
+        this.onTaskListChanged = callback;
+    }
+    _commit(tasks) {
+        this.onTaskListChanged(tasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
     add(task) {
         this.tasks.push(new Task(task));
-        localStorage.setItem('tasks', this.tasks);
+        this._commit(this.tasks);
     }
     edit(_task) {
         this.tasks = this.tasks.map(task => task.id === _task.id ? new Task(_task) : task);
-        localStorage.setItem('tasks', this.tasks);
+        this._commit(this.tasks);
     }
     remove(_id) {
         this.tasks = this.tasks.filter(({id}) => id !== _id);
-        localStorage.setItem('tasks', this.tasks);
+        this._commit(this.tasks);
     }
-
+    complete(_id) {
+        this.tasks = this.tasks.map(task => task.id === _task.id ? new Task(_task) : task);
+        this._commit(this.tasks);
+    }
+    important(_id) {
+        this.tasks = this.tasks.map(task => task.id === _task.id ? new Task(_task) : task);
+        this._commit(this.tasks);
+    }
 }
